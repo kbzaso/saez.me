0b4566b06aaabde34138e502c1ed71bf982578cd
kbzaso <alejandro.sapa@gmail.com>
2022-01-12 13:39:49 -0300

    feat: Add Tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-auto'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +7,10 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+		target: '#svelte',
+	},
 
-export default config;
+	preprocess: [preprocess({})],
+}
+
+export default config
